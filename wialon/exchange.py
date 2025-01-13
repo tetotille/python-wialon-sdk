bdee62c2217375e126228db21d833e10dc12101f
Jorge Tilleria <jltilleriam@gmail.com>
2025-01-13 09:45:33 -0300

    ✨ feat(exchange): add import_messages method and update parameter types for export_messages_by_layer and export_messages_by_id

--- wialon/exchange.py
+++ wialon/exchange.py
@@ -1,15 +1,40 @@
 from datetime import datetime
+from typing import Any, Optional
 
 class FormatException(Exception):
     """Error code 2015: Invalid format"""
     pass
 
 class Exchange:
-    def __init__(self,engine):
+    def __init__(self,engine:Any):
         self._engine = engine
         self._formats = ["txt","kml","plt","wln","wlb"]
+
+    def import_messages(self,unit_id:int,filepath:str,event_hash:Optional[str]=None):
+        """
+        Import messages for a specific unit from a file.
+
+        Args:
+            unit_id (int): The ID of the unit to import messages for.
+            filepath (str): The path to the file containing the messages to import.
+            event_hash (str, optional): A hash of the event. Defaults to None.
+
+        Returns:
+            dict: The result of the import operation.
+        """
+        svc = "exchange/import_messages"
+        params = {
+            "itemId":unit_id,
+            "eventHash":event_hash
+        }
+        with open(filepath,"rb") as f:
+            files = {
+                "upload_file": f
+            }
+            response = self._engine.request(svc,params,sid=self._engine.auth.get_sid(),send_file=files)
+        return response
     
-    def export_messages_by_layer(self,layer_name:str,file_format:str,filepath:str=None,compress:bool=False):
+    def export_messages_by_layer(self,layer_name:str,file_format:str,filepath:Optional[str]=None,compress:bool=False):
         """
         Export messages by layer.
 
@@ -39,7 +64,7 @@ class Exchange:
                 f.write(result)
         return result
     
-    def export_messages_by_id(self,unit_id:int,date_from:datetime,date_to:datetime,file_format:str,filepath:str=None,compress:bool=False):
+    def export_messages_by_id(self,unit_id:int,date_from:datetime,date_to:datetime,file_format:str,filepath:Optional[str]=None,compress:bool=False):
         """
         Export messages for a specific unit within a given time range.
 
