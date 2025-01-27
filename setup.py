4ed8874f02b0afa44c55043061d0dd3a370f24d0
Jorge Tilleria <jltilleriam@gmail.com>
2025-01-27 11:17:40 -0300

    🔖 chore(setup): update version to 0.3.1 in setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
   name='wialon-sdk',
-  version='0.2.4',
+  version='0.3.1',
   packages=find_packages(),
   install_requires=[
     'requests>=2.32.3'
