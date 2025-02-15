a3c139a80fee5685a7c8b5e54878eb7726f4c823
Jorge Tilleria <jltilleriam@gmail.com>
2025-02-15 14:55:25 -0300

    🎨 refactor(init): set to the ruff format

--- wialon/__init__.py
+++ wialon/__init__.py
@@ -1,8 +1,22 @@
+"""Wialon SDK for Python."""
+
 from .auth_manager import AuthManager
-from .errors import validate_error,SessionException
+from .errors import SessionExceptionError, validate_error
 from .exchange import Exchange
 from .extra import Extra
 from .items import Items
 from .messages import Messages
 from .renderer import Render
 from .wialon import Wialon
+
+__all__ = [
+    "AuthManager",
+    "Exchange",
+    "Extra",
+    "Items",
+    "Messages",
+    "Render",
+    "SessionExceptionError",
+    "Wialon",
+    "validate_error",
+]
