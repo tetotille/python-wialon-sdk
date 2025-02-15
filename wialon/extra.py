c51c5451d2b464a21025df87f522ba9772438586
Jorge Tilleria <jltilleriam@gmail.com>
2025-02-15 16:26:38 -0300

    🎨 refactor(extra): enhance Extra class with docstrings and type hints

--- wialon/extra.py
+++ wialon/extra.py
@@ -1,27 +1,44 @@
-from typing import TYPE_CHECKING, Any,Any as Wialon, Dict, List
+"""This module contains the Extra class."""
+
+from typing import TYPE_CHECKING, Any
+from typing import Any as Wialon
 
 if TYPE_CHECKING:
     from . import Wialon
 
+
 class Extra:
-    def __init__(self,engine:Wialon):
+    """The Extra class is used to perform extra requests to the Wialon API."""
+
+    def __init__(self, engine: "Wialon") -> None:
+        """Initialize the Extra class.
+
+        :param engine: The Wialon object.
+        :type engine: Wialon
+        """
         self._engine = engine
 
-    def batch(self,params:List[Dict[str,Any]]) -> List[Dict[str,Any]]:
-        """
-        Perform a batch request.
+    def batch(self, params: list[dict[str, Any]]) -> list[dict[str, Any]]:
+        """Perform a batch request.
 
         Parameters
         ----------
         params : list of dict
             The parameters for each request in the batch.
 
-        Returns
+        Returns:
         -------
         list of dict
             The results of each request in the batch.
         """
-        result = self._engine.request("core/batch",params,self._engine.auth.get_sid())
-        if type(result) != list:
-            raise ValueError("Invalid response from the server.")
+        response = self._engine.request(
+            "core/batch",
+            params,
+            self._engine.auth.get_sid(),
+        )
+
+        if not isinstance(response, list):
+            msg = "Invalid response from the server."
+            raise TypeError(msg)
+        result: list[dict[str, Any]] = response
         return result
