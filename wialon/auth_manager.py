e829ce28d81ea86962519eb59389a1432aa88f4d
Jorge Tilleria <jltilleriam@gmail.com>
2025-02-15 14:58:25 -0300

    🎨 refactor(auth): enhance AuthManager class with type hints and docstrings

--- wialon/auth_manager.py
+++ wialon/auth_manager.py
@@ -1,96 +1,151 @@
-from .errors import SessionException
-from typing import Any,Union
+"""AuthManager class."""
+
+from typing import TYPE_CHECKING, Any
+
+from .errors import SessionExceptionError
+
+if TYPE_CHECKING:
+    from .wialon import Wialon
 
 
 class AuthManager:
-    def __init__(self,token:str,engine:Any):
+    """AuthManager class."""
+
+    def __init__(self, token: str, engine: "Wialon") -> None:
+        """Initialize the AuthManager class.
+
+        :param token: The authentication token.
+        :type token: str
+        :param engine: The Wialon engine instance.
+        :type engine: Wialon
+        """
         self.token = token
         self._engine = engine
         self._login()
         self._access_types = {
-            "general": 0xffff,
+            "general": 0xFFFF,
             "units": 0xCCF7F00000,
             "users": 0x1F00000,
             "retranslator": 0x300000,
             "resources": 0x301FFFF00000,
             "routes": 0x100000,
-            "all": 0xfffffffffffffff
+            "all": 0xFFFFFFFFFFFFFFF,
         }
+        self.host = None
+        self._sid = ""
+        self.api_type = None
+        self.version = None
+        self.user_name = None
+        self.user_id = None
 
-    def login(self,token:str):
+    def login(self, token: str) -> None:
+        """Login to the Wialon API.
+
+        :param token: The authentication token.
+        :type token: str
+        """
         self.token = token
         self._login()
-        
-    def _login(self):
+
+    def _login(self) -> None:
+        """Login to the Wialon API."""
         svc = "token/login"
-        params = {"token":self.token,"fl":"2"}
-        response = self._engine.request(svc=svc,params=params)
-        
+        params = {"token": self.token, "fl": "2"}
+        response: dict[
+            str,
+            str | int | dict[str, str | int | dict[str, str | int | bool]],
+        ] = (  # type: ignore[no-untyped-call]
+            self._engine.request(
+                svc=svc,
+                params=params,
+            )
+        )
+
         self.host = response["host"]
-        self._sid = response["eid"]
+        if response["eid"] is str:
+            self._sid: str = response["eid"]
         self.api_type = response["api"]
         self.version = response[f"{self.api_type}_version"]
-        self.user_name = response["user"]["nm"]
-        self.user_id = response["user"]["id"]
+        if type(response["user"]) is dict:
+            self.user_name = response["user"]["nm"]
+            self.user_id = response["user"]["id"]
 
-    def account_detail(self,detailed:Union[int,bool]=False):
-        """
-        Retrieve account details.
+    def account_detail(
+        self,
+        detailed: int = 0,
+    ) -> dict[str, Any] | list[dict[str, Any]] | bytes:
+        """Retrieve account details from the Wialon API.
 
-        Args:
-            detailed (bool, optional): If True, retrieves detailed account information. Defaults to False.
-
-        Returns:
-            dict: The account data retrieved from the service.
+        :param detailed: Flag to indicate whether to retrieve detailed account
+        :type detailed: int, optional
+                         information. Defaults to 0.
+        :return: Account details from the Wialon API.
+        :rtype: dict
         """
         svc = "core/get_account_data"
         detailed = 1 if detailed else 0
-        params = {"type":detailed}
-        return self._engine.request(svc=svc,params=params,sid=self.get_sid())
-        
-    def check_access(self,itemsId:list[int],accessType:str,service_name:str="*",flags:int=0):
-        """
-        Check access permissions for specified items.
+        params = {"type": str(detailed)}
+        return self._engine.request(svc=svc, params=params, sid=self.get_sid())
 
-        Args:
-            itemsId (list[int]): List of item IDs to check access for.
-            accessType (str): Type of access to check. Must be one of the predefined access types:
-                - general: General access
-                - units: Access to units
-                - users: Access to users
-                - retranslator: Access to retranslator
-                - resources: Access to resources
-                - routes: Access to routes
-                - all: Access to all
-            service_name (str, optional): Name of the service requesting access. Defaults to "*".
-            flags (int, optional): Flags to modify access check behavior. Defaults to 0.
+    def check_access(
+        self,
+        items_id: list[int],
+        access_type: str,
+        service_name: str = "*",
+        flags: int = 0,
+    ) -> dict[str, Any] | list[dict[str, Any]] | bytes:
+        """Check access permissions for specified items.
 
-        Raises:
-            ValueError: If the provided accessType is not valid.
-
-        Returns:
-            dict: Response from the engine request indicating access permissions.
+        :param items_id: The items ID.
+        :type items_id: list[int]
+        :param access_type: The access type.
+        :type access_type: str
+        :param service_name: The service name.
+        :type service_name: str, optional
+        :param flags: The flags.
+        :type flags: int, optional
+        :return: The access permissions for the specified items.
+        :rtype: dict
         """
         svc = "core/check_items_billing"
-        if accessType not in self._access_types: raise ValueError(f"Invalid access type: {accessType}")
+        if access_type not in self._access_types:
+            msg = f"Invalid access type: {access_type}"
+            raise ValueError(msg)
         params = {
-            "itemId":itemsId,
-            "accessType": flags if flags else self._access_types[accessType],
-            "serviceName":service_name
+            "itemId": items_id,
+            "accessType": flags if flags else self._access_types[access_type],
+            "serviceName": service_name,
         }
-        return self._engine.request(svc=svc,params=params,sid=self.get_sid())
-        
-    def logout(self):
+        return self._engine.request(svc=svc, params=params, sid=self.get_sid())
+
+    def logout(self) -> dict[str, Any] | list[dict[str, Any]] | bytes:
+        """Logout from the Wialon API.
+
+        :raises SessionException: If there is no active session.
+        :return: The response from the API.
+        :rtype: dict[str, Any] | list[dict[str, Any]] | bytes
+        """
         svc = "core/logout"
         if self.get_sid():
-            response = self._engine.request(svc=svc,sid=self.get_sid())
-            self._sid = None
+            response = self._engine.request(svc=svc, sid=self.get_sid())
+            self._sid = ""
             return response
-        else:
-            raise SessionException("There is no active session")
-        
-    def get_sid(self):
+
+        msg = "There is no active session"
+        raise SessionExceptionError(msg)
+
+    def get_sid(self) -> str:
+        """Get the session ID.
+
+        :return: The session ID.
+        :rtype: str
+        """
         return self._sid
-    
-    def __str__(self):
+
+    def __str__(self) -> str:
+        """Return the string representation of the AuthManager object.
+
+        :return: The string representation of the AuthManager object.
+        :rtype: str
+        """
         return f"name:{self.user_name}\nsid: {self._sid}"
