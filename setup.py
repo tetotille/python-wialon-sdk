61460fc172ab465f3c488b747d9b0bcdc2a37a31
Jorge Tilleria <jltilleriam@gmail.com>
2025-02-16 23:54:16 -0300

    🔧 chore(setup.py): update package version to 0.4.0 and add python-dotenv dependency

--- setup.py
+++ setup.py
@@ -1,27 +1,29 @@
-from setuptools import setup, find_packages
+"""Setup script for the package."""
+
+from setuptools import find_packages, setup
 
 setup(
-  name='wialon-sdk',
-  version='0.3.1',
-  packages=find_packages(),
-  install_requires=[
-    'requests>=2.32.3'
-  ],
-  entry_points={
-    'console_scripts': [
-      'wialon-sdk=wialon_sdk.__main__:main'
+    name="wialon-sdk",
+    version="0.4.0",
+    packages=find_packages(),
+    install_requires=[
+        "requests>=2.32.3",
     ],
-  },
-  author='Jorge Tillería',
-  author_email='jltilleriam@gmail.com',
-  description='Python SDK for Wialon API',
-  long_description=open('README.md').read(),
-  long_description_content_type='text/markdown',
-  url='https://github.com/tetotille/wialon-sdk',
-  classifiers=[
-    'Programming Language :: Python :: 3',
-    'License :: OSI Approved :: MIT License',
-    'Operating System :: OS Independent',
-  ],
-  python_requires='>=3.8',
+    entry_points={
+        "console_scripts": [
+            "wialon-sdk=wialon_sdk.__main__:main",
+        ],
+    },
+    author="Jorge Tillería",
+    author_email="jltilleriam@gmail.com",
+    description="Python SDK for Wialon API",
+    long_description=open("README.md").read(),  # noqa: PTH123, SIM115
+    long_description_content_type="text/markdown",
+    url="https://github.com/tetotille/wialon-sdk",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.8",
 )
